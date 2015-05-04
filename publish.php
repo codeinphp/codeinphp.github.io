c89aed1c418ed8eae331c8725cf9cb2dccbb3095
Sarfraz <sarfraznawaz2005@gmail.com>
2015-05-05 02:26:06 +0500

    updated blog

---
+++ publish.php
@@ -0,0 +1,17 @@
+<?php
+ini_set('output_buffering', false);
+ini_set('implicit_flush', 'true');
+
+//exec('git pull 2>&1', $output);
+exec('git add . 2>&1', $output);
+exec('git commit -am "updated blog" 2>&1', $output);
+exec('git push 2>&1', $output);
+
+foreach($output as $line) {
+  if (!trim($line)) continue;
+  
+  ob_flush();
+  echo $line . '<br>';
+  flush();
+  sleep (1);
+}  
