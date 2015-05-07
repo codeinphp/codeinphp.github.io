d31371d2b356dbfeb54b8630b9bc1d8ce0341dab
Sarfraz <sarfraznawaz2005@gmail.com>
2015-05-07 20:51:48 +0500

    updated blog

--- publish.php
+++ publish.php
@@ -6,7 +6,7 @@ set_time_limit(0);
 ini_set('output_buffering', false);
 ini_set('implicit_flush', 'true');
 
-//exec('git pull 2>&1', $output);
+exec('git pull 2>&1', $output);
 exec('git add . 2>&1', $output);
 exec('git commit -am "updated blog" 2>&1', $output);
 exec('git push 2>&1', $output);
