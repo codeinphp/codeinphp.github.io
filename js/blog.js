91dd5c786fd46dfbb40be3fd5fd718c36b2f8aa9
Sarfraz <sarfraznawaz2005@hotmail.com>
2022-02-06 16:52:44 +0500

    Update blog.js

--- js/blog.js
+++ js/blog.js
@@ -1,8 +1,4 @@
-/**
- * Created by Sarfraz on 4/3/2015.
- */
-
-$(function () {
+document.addEventListener("DOMContentLoaded", function(event) {
     if (window.location.href.indexOf("post/") > -1) {
 	    window.location.href = 'https://codenphp.blogspot.com/search?q=' + encodeURIComponent(window.location.href.split('post/')[1].replace(/[\W_]+/g," "));
     }
